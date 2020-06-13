03315039065ac158a3fa2891d2f65ad01a0fa557
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-06-13 21:13:07 +0530

    reformatting the ProjectAlbumView.js file

--- src/Components/ProjectAlbumView.js
+++ src/Components/ProjectAlbumView.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Grid, Card, CardMedia, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
 
 import Img from "../Assets/ChetanBhogade.jpg";
 
@@ -10,12 +19,12 @@ const useStyle = makeStyles((theme) => ({
     paddingBottom: theme.spacing(8),
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: "56.25%", // 16:9
   },
   cardContent: {
     flexGrow: 1,
@@ -31,33 +40,36 @@ function ProjectAlbumView() {
     <React.Fragment>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid container spacing={4}>
-          {
-            data.map((item, index) => {
-              return (
-                <Grid item key={index} xs={12} sm={6} md={4}>
-                  <Card className={classes.card}>
-                    <CardMedia className={classes.cardMedia} image={Img} title="Image Title" />
-                    <CardContent className={classes.cardContent} >
-                      <Typography gutterBottom variant="h5" component="h2">
-                        Heading {item}
-                      </Typography>
-                      <Typography>
-                      This is a media card. You can use this section to describe the content.
-                      </Typography>
-                    </CardContent>
-                    <CardActions>
-                      <Button size="small" color="primary">
-                        View
-                      </Button>
-                      <Button size="small" color="primary">
-                        Edit
-                      </Button>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              )
-            })
-          }
+          {data.map((item, index) => {
+            return (
+              <Grid item key={index} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={Img}
+                    title="Image Title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading {item}
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
       </Container>
     </React.Fragment>
