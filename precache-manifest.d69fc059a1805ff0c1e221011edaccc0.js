08e68933f99bb651b5c039ddff2b0a9db22a49cd
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-06-15 08:26:04 +0530

    Updates

---
+++ precache-manifest.d69fc059a1805ff0c1e221011edaccc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f051c65199a6003bf64b1b48bb64e2ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2e628c4c7a5febf5090c",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "428ad3aaa0d9649ea4ba",
+    "url": "/portfolio/static/css/main.62130b4e.chunk.css"
+  },
+  {
+    "revision": "2e628c4c7a5febf5090c",
+    "url": "/portfolio/static/js/2.4d41d929.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/portfolio/static/js/2.4d41d929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "428ad3aaa0d9649ea4ba",
+    "url": "/portfolio/static/js/main.321e9b2d.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "b55e645291eac9672fc41d2c0c1dc325",
+    "url": "/portfolio/static/media/CardMemoryGame.b55e6452.png"
+  },
+  {
+    "revision": "d701949579a1dce6503a3c49547ed3c3",
+    "url": "/portfolio/static/media/ChetanBhogade.d7019495.jpg"
+  },
+  {
+    "revision": "1eedb5967aef30b0199a0971e5316517",
+    "url": "/portfolio/static/media/EventCountdown.1eedb596.png"
+  },
+  {
+    "revision": "96faae6fed54e30575ebfd0e73952a55",
+    "url": "/portfolio/static/media/FaceProfile.96faae6f.jpg"
+  },
+  {
+    "revision": "abaf0b5c245ee71d93c089c3cee8c72e",
+    "url": "/portfolio/static/media/NccManagement.abaf0b5c.png"
+  },
+  {
+    "revision": "5596262c8483841b9f32128a0a6e2f3a",
+    "url": "/portfolio/static/media/eComm.5596262c.png"
+  }
+]);
