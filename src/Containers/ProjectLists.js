0fcba48c6dd15a76e80a16b0cef450943c8b9569
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-08-07 00:06:50 +0530

    removing unneccessary code with hardcoded data

--- src/Containers/ProjectLists.js
+++ src/Containers/ProjectLists.js
@@ -5,92 +5,13 @@ import { CircularProgress } from "@material-ui/core";
 
 import "./ProjectList.css";
 
-import CardGameImg from "../Assets/Projects/CardMemoryGame.png";
-import eCommImg from "../Assets/Projects/eComm.png";
-import EventImg from "../Assets/Projects/EventCountdown.png";
-import NccImg from "../Assets/Projects/NccManagement.png";
-import TtsImg from "../Assets/Projects/TextToSpeech.png";
-import RedFruitImg from "../Assets/Projects/RedFruitAgro.png";
-import IpInfo from "../Assets/Projects/IP-Information.jpg";
-
-const data = [
-  {
-    name: "Red Fruit Agro",
-    description:
-      "A Website with the integrstion of React and Django. Contains Rest API, Redux, Custom User Model, Custom User Authentication and many More",
-    image: RedFruitImg,
-    visit: "https://redfruitagro.com/",
-    src: null,
-    language: ["ReactJs", "Django", "Python", "Django-Rest", "JavaScript"],
-  },
-  {
-    name: "E-Commerce Website - 'eComm'",
-    description:
-      "Basic eCommerce website with the implementation of some Analytics based on views, time and amount purchased.",
-    image: eCommImg,
-    visit: "http://chetanbhogade.pythonanywhere.com/",
-    src: "https://github.com/ChetanBhogade/E-Commerce-Project",
-    language: ["Django", "Python", "JavaScript"],
-  },
-  {
-    name: "Card-Memory Game",
-    description:
-      "A game where you have to click on a card to see what icon is underneath it and try to find the matching icon underneath the other cards.",
-    image: CardGameImg,
-    visit: "https://chetanbhogade.github.io/card-memory-game/",
-    src: "https://github.com/ChetanBhogade/card-memory-game",
-    language: ["ReactJs", "JavaScript"],
-  },
-  {
-    name: "Event Countdown",
-    description:
-      "Continuously decrementing countdown timer to a user entered event.",
-    image: EventImg,
-    visit: "https://chetanbhogade.github.io/Event-Countdown/",
-    src: "https://github.com/ChetanBhogade/Event-Countdown",
-    language: ["ReactJs", "JavaScript"],
-  },
-  {
-    name: "Ncc Management",
-    description:
-      "All Ncc related paper works are try to make digital. All CRUD operations are performed.",
-    image: NccImg,
-    visit: null,
-    src: "https://github.com/ChetanBhogade/NCC-Management",
-    language: ["Asp.Net"],
-  },
-  {
-    name: "Text-To-Speech",
-    description:
-      "This software can read text from input field and can also read whole text file.",
-    image: TtsImg,
-    visit: null,
-    src:
-      "https://github.com/ChetanBhogade/TextToSpeech/blob/master/TextToSpeech.py",
-    language: ["Python"],
-  },
-  {
-    name: "IP-Information",
-    description:
-      "Mobile application for getting an IP address information on android and iOS.",
-    image: IpInfo,
-    visit: null,
-    src: "https://github.com/ChetanBhogade/ip-information",
-    language: ["JavaScript", "React-Native"],
-  },
-];
-
 function ProjectLists() {
   const [myData, setMyData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  console.log(myData);
-
   useEffect(() => {
-    console.log("effect called...");
-    const starCountRef = firebase.database().ref("portfolio/");
+    const starCountRef = firebase.database().ref("project");
     starCountRef.on("value", (snapshot) => {
-      // console.log(snapshot.val())
       snapshot.forEach((childSnapshot) => {
         // key will be "ada" the first time and "alan" the second time
         const key = childSnapshot.key;
